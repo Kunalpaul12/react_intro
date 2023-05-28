94a828038803103aa52aeb839be2aa57bad3e755
Kunal_Paul <kunalletstalk@gmail.com>
2023-05-28 22:09:30 +0530

    Added timer example

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import Timer from "./Tutorial";
+
 function App() {
   return (
     <div className="App">
       <h1> React Intro</h1>
+      <Timer />
     </div>
   );
 }
--- src/Tutorial/index.js
+++ src/Tutorial/index.js
@@ -1,1 +1,22 @@
-///// ADD YOUR TUTORIAL CODE HERE /////////////
+import React, { useEffect, useState } from "react";
+
+const Timer = () => {
+  const [timer, setTimer] = useState(0);
+
+  useEffect(() => {
+    const timerInterval = setInterval(() => {
+      setTimer(timer + 1);
+    }, 1000);
+    return () => {
+      clearInterval(timerInterval);
+    };
+  });
+
+  return (
+    <div>
+      <h1>{timer}</h1>
+    </div>
+  );
+};
+
+export default Timer;

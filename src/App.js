3477f51dacc334a51a0aaf9cb314e1a8905520fe
Kunal_Paul <kunalletstalk@gmail.com>
2023-06-06 11:05:49 +0530

    useImperative hook example

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import TopContainer from "./Tutorial/index";
+
 function App() {
   return (
     <div className="App">
       <h1> React Intro</h1>
+      <TopContainer />
     </div>
   );
 }
--- src/Tutorial/index.js
+++ src/Tutorial/index.js
@@ -1,1 +1,19 @@
-///// ADD YOUR TUTORIAL CODE HERE /////////////
+import { useRef } from "react";
+import CustomInput from "./customInputField";
+
+export default function Form() {
+  const ref = useRef(null);
+
+  function handleClick() {
+    ref.current.focus();
+  }
+
+  return (
+    <form>
+      <CustomInput label="Enter your name:" ref={ref} />
+      <button type="button" onClick={handleClick}>
+        Edit
+      </button>
+    </form>
+  );
+}
